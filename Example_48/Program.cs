e4647931df80b0ecd00f8857bf32ff4916b56cc4
wargie <wargie@bk.ru>
2022-02-25 09:14:40 +0200

    Оптимизация кода к задаче №48

--- Example_48/Program.cs
+++ Example_48/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 48: Показать двумерный массив размером m×n заполненный целыми числами
+
+int[,] GetTable(int row, int col)
+{
+    int[,] table = new int[row, col];
+    for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+                {
+                    table[i, j] = new Random().Next(-100, 101);
+                }
+        }
+    return table;
+}
+
+void PrintTable(int[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+        {
+            for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    Console.Write(tab[i, j] + " ");
+                }
+            Console.WriteLine();
+        }
+}
+
+int row = new Random().Next(2, 30);
+int col = new Random().Next(2, 30);
+
+int[,] arr = GetTable(row, col);
+PrintTable(arr);
